3779c96b71890897521a5961b6d85bc81ec1609f
Nick Anthony <nanthony007@gmail.com>
2021-04-27 09:27:18 -0400

    Adjust to get most recent 50,000 records.

--- web/js/CDC-Covid-19-Surveillance.js
+++ web/js/CDC-Covid-19-Surveillance.js
@@ -113,24 +113,44 @@
 	// Web Data Connector.
 	myConnector.getData = function (table, doneCallback) {
 		var baseURL = 'https://data.cdc.gov/resource/n8mc-b4w4.json';
+		var newURL = '';
 
-		Promise.all(
-			[...Array(22500).keys()].map((n) => {
-				return fetchAPI(n * 1000, baseURL);
-			})
-		).then((data) => {
-			var final = [];
-			Object.keys(data).forEach((k) => {
-				data[k].forEach((r) => {
-					final.push(r);
-				});
-			});
-			table.appendRows(final);
-			doneCallback();
-		});
+		myConnector.getData = function (table, doneCallback) {
+			$.getJSON(
+				'https://data.cdc.gov/resource/n8mc-b4w4.json?$limit=50000&$order=case_month%20DESC',
+				function (resp) {
+					var tableData = [];
+
+					for (var i = 0, len = resp.length; i < len; i++) {
+						tableData.push({
+							case_month: resp[i].case_month,
+							res_state: resp[i].res_state,
+							state_fips_code: resp[i].state_fips_code,
+							res_county: resp[i].res_county,
+							county_fips_code: resp[i].county_fips_code,
+							age_group: resp[i].age_group,
+							sex: resp[i].sex,
+							race: resp[i].race,
+							ethnicity: resp[i].ethnicity,
+							case_onset_interval: resp[i].case_onset_interval,
+							process: resp[i].process,
+							exposure_yn: resp[i].exposure_yn,
+							current_status: resp[i].current_status,
+							symptom_status: resp[i].symptom_status,
+							exposure_yn: resp[i].exposure_yn,
+							hosp_yn: resp[i].hosp_yn,
+							icu_yn: resp[i].icu_yn,
+							death_yn: resp[i].death_yn,
+						});
+					}
+					table.appendRows(tableData);
+					doneCallback();
+				}
+			);
+		};
 	};
 
-	// This is reqired to register the Web Data Connector.
+	// This is required to register the Web Data Connector.
 	tableau.registerConnector(myConnector);
 
 	// Once the document has loaded we will attached functionality
